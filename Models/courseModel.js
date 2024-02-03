67e575b7d03937765350f8ab576cb18d600e42e4
SubhamPaul21 <subhampaul12@gmail.com>
2024-02-03 17:05:58 +0530

    added course schema, model and validation methods

--- Models/courseModel.js
+++ Models/courseModel.js
@@ -23,10 +23,24 @@ async function validateCourse(course) {
         creator: Joi.string().min(3).max(30).required().alphanum(),
         rating: Joi.number().min(0).max(5).required(),
         category: Joi.string().valid(...categoryArray).required(),
+    });
+    return courseSchema.validate(course);
+}
 
+// Function to validate new Course Input
+async function validateExistingCourse(_id, course) {
+    const categoryArray = await getAndUpdateCategoryArray();
+    const existingCourse = await Course.findById(_id);
+    const courseSchema = Joi.object({
+        name: Joi.string().min(3).max(30).alphanum().default(existingCourse.name),
+        description: Joi.string().min(10).max(100).default(existingCourse.description),
+        creator: Joi.string().min(3).max(30).alphanum().default(existingCourse.creator),
+        rating: Joi.number().min(0).max(5).default(existingCourse.rating),
+        category: Joi.string().valid(...categoryArray).default(existingCourse.category),
     });
     return courseSchema.validate(course);
 }
 
 exports.Course = Course;
 exports.validate = validateCourse;
+exports.validateExistingCourse = validateExistingCourse;
