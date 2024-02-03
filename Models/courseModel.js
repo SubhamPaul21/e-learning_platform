07be57158fbdd18b27f61e1292365f8f92ec6d3d
SubhamPaul21 <subhampaul12@gmail.com>
2024-02-03 16:42:29 +0530

    added course schema, model and validation method

--- Models/courseModel.js
+++ Models/courseModel.js
@@ -1,24 +1,31 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
+const { getAndUpdateCategoryArray } = require('../utils/getAndUpdate_CategoryArray');
 
-// Generate Course Schema
-const courseSchema = {
-    name: Joi.string().min(3).max(30).required().alphanum(),
-    description: Joi.string().min(10).max(100).required().alphanum(),
-    creator: Joi.string().min(3).max(30).required().alphanum(),
-    rating: Joi.number().min(0).max(5).required(),
-    category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category'
-    }
-}
+// Create Course Schema
+const courseSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    creator: String,
+    rating: Number,
+    category: String,
+})
 
 // Create Course Model
 const Course = mongoose.model('Course', courseSchema);
 
 // Function to validate new Course Input
-function validateCourse(course) {
-    return Joi.object(courseSchema).validate(course);
+async function validateCourse(course) {
+    const categoryArray = await getAndUpdateCategoryArray();
+    const courseSchema = Joi.object({
+        name: Joi.string().min(3).max(30).required().alphanum(),
+        description: Joi.string().min(10).max(100).required(),
+        creator: Joi.string().min(3).max(30).required().alphanum(),
+        rating: Joi.number().min(0).max(5).required(),
+        category: Joi.string().valid(...categoryArray).required(),
+
+    });
+    return courseSchema.validate(course);
 }
 
 exports.Course = Course;
